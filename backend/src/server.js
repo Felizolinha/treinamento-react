73b5085b7663ee0000bf241232d7b6b805079826
Matheus Felizola Freires <matheusbsb2009@hotmail.com>
2019-09-06 00:41:08 -0300

    Added mongoose and CORS to server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const cors = require("cors");
 const routes = require("./routes.js");
 
+const config = require("../config");
+
 const server = express();
+mongoose.connect(config.mongodbServer, {
+  useNewUrlParser: true
+});
+
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
