2bc233dfe0cdef47d6cafd1e9f2e140bf6c324f6
Matheus Felizola Freires <matheusbsb2009@hotmail.com>
2019-09-05 21:41:09 -0300

    Created route GET /login

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const routes = express.Router();
 
+routes.get("/login", (req, res) => {
+  const { username, password } = req.query;
+  res
+    .json({
+      userId: username + password
+    })
+    .send(200);
+});
+
 module.exports = routes;
